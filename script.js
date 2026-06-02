eaed4cff2ce90086e8cd754ea1f46a26fd8fd361
Victor Pereira <victoor.pereiraa@hotmail.com>
2026-06-02 19:37:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Flappy Bird');
 
 const sprites = new Image();
-sprites.src = 'script.js';
+sprites.src = 'sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
