75e78d57554cd6fe2c1761980c8d677304235b98
Victor Pereira <victoor.pereiraa@hotmail.com>
2026-06-02 19:16:27 -0300

    Update script
    Escrita e busca da pagina script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-console.log('[Flappy Bird');
+console.log('Flappy Bird');
 
 const sprites = new Image();
-sprites.src = './sprites.png';
+sprites.src = 'script.js';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
